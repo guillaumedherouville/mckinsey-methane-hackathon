972a34fe7a093b62f2d3640598ec54c21eb0630e
betapekens <dikens2000@gmail.com>
2023-10-12 14:14:29 +0200

    Deleted import of visual keras

--- app/methane_detect.py
+++ app/methane_detect.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import numpy as np
-import cv2 as cv
 import os
 from tensorflow import keras
-import visualkeras
 import matplotlib.pyplot as plt
 from PIL import Image
 import io
