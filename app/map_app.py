13eedaf1bcfb2113b1a0394230286266f8edd85d
Rehann2 <116149355+Rehann2@users.noreply.github.com>
2023-10-12 13:53:43 +0200

    Log files

--- app/map_app.py
+++ app/map_app.py
@@ -1,51 +1,58 @@
 import streamlit as st
 import pandas as pd
 
-def display_city_name(city):
-    st.markdown(f"Selected City: {city}")
+# Function to display a map with plume data
 
-def display_map_with_location(latitude, longitude):
-    st.map(data={"LAT": [latitude], "LON": [longitude]})
-    st.markdown(f"Latitude: {latitude}, Longitude: {longitude}")
+
+def display_map_with_plume(df):
+    st.map(df[df['plume'] == 'yes'])
+
+# Function to display a map without plume data
+
+
+def display_map_without_plume(df):
+    st.map(df[df['plume'] == 'no'])
+
+# Streamlit app
+
 
 def historical_data():
     st.title("Historical Data")
 
-    df = pd.read_csv("data/dataset/train_data/metadata.csv")
-    plume_yes_df = df[df['plume'] == 'yes'].drop_duplicates()
-    plume_no_df = df[df['plume'] == 'no'].drop_duplicates()
+    df = pd.read_csv("data/dataset/train_data/metadata.csv").drop_duplicates()
 
-    # Create a Streamlit web app
     st.title("Map with Plume Data")
 
-    # Add a selectbox to allow the user to choose between "plume = yes" and "plume = no"
-    option = st.selectbox("Select data to display:", ("Show locations with plume", "Show locations without plume"))
+    if st.button("Show locations with plume"):
+        display_map_with_plume(df)
+    if st.button("Show locations without plume"):
+        display_map_without_plume(df)
 
-    if option == "Show locations with plume":
-        st.map(plume_yes_df, use_container_width=True)  # Display the map with plume = yes
-    else:
-        st.map(plume_no_df, use_container_width=True)  # Display the map with plume = no
-
-        # Read the CSV file
     cities = pd.read_csv('app/locations_with_cities.csv')
 
-    # Extract unique cities from the 'city' column
     unique_cities = cities['city'].unique()
-    unique_cities = ['Select Location', "Add New Location",] + [city for city in unique_cities if str(city) not in ['N/A', 'nan']]
+    unique_cities = ['Select Location', "Add New Location",] + \
+        [city for city in unique_cities if str(city) not in ['N/A', 'nan']]
 
-
-    # User input: choose city from a dropdown or set location with latitude and longitude
     location_option = st.selectbox("Choose an option:", unique_cities)
 
     if location_option == "Select Location":
         pass
     elif location_option == "Add New Location":
-        # User input for latitude and longitude
-        default_latitude = 48.8566  # Default latitude for Paris
-        default_longitude = 2.3522  # Default longitude for Paris
+        default_latitude = 48.8566
+        default_longitude = 2.3522
         latitude = st.number_input("Enter Latitude:", value=default_latitude)
-        longitude = st.number_input("Enter Longitude:", value=default_longitude)
-        display_map_with_location(latitude, longitude)
-        
+        longitude = st.number_input(
+            "Enter Longitude:", value=default_longitude)
+        st.map({"lat": [latitude], "lon": [longitude]})
     else:
-        display_city_name(location_option)
+        # Zoom into the selected city
+        selected_city_data = cities[cities['city'] == location_option]
+        if not selected_city_data.empty:
+            city_latitude = selected_city_data.iloc[0]['lat']
+            city_longitude = selected_city_data.iloc[0]['lon']
+            st.map({"lat": [city_latitude], "lon": [city_longitude]})
+
+
+if __name__ == '__main__':
+    historical_data()
