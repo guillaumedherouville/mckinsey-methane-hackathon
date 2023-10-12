bd962e211d3d7f0b587c9d832579697b756ec9ff
Alekseyev.Mykyta <mykyta.alekseyev@hneu.net>
2023-10-12 12:16:17 +0200

    Added new submission file, which brought the best score (95)

--- training_scripts/training_pipeline.py
+++ training_scripts/training_pipeline.py
@@ -8,7 +8,9 @@ import argparse
 
 import torch
 from torch import nn
+from torch.nn.functional import sigmoid
 from torch.utils.data import DataLoader
+from sklearn.metrics import roc_auc_score
 
 from utils.data import PlumesDataset
 
@@ -25,16 +27,23 @@ def _define_device():
     return device
 
 
-def _train_epoch(model, dataloader, criterion, optimizer):
+def _train_epoch(model, dataloader, criterion, optimizer, device):
     train_set_size = len(dataloader.dataset)
     correct_predictions = 0
+
+    all_targets = list()
+    predicted_probabilities = list()
 
     model.train()
     for batch_idx, (X, y_truth) in enumerate(dataloader):
         # Compute loss on a given batch.
-        y_truth = y_truth.unsqueeze(1)
+        X = X.to(device)
+        y_truth = y_truth.unsqueeze(1).to(device)
         y_pred = model(X)
         batch_loss = criterion(y_pred, y_truth)
+
+        # Compute predicted probability.
+        y_pred = sigmoid(y_pred)
 
         # Update weights.
         batch_loss.backward()
@@ -44,45 +53,65 @@ def _train_epoch(model, dataloader, criterion, optimizer):
         # Accuracy.
         correct_predictions += (y_truth == (y_pred >= 0.5)).type(torch.float).sum().item()
 
+        # ROC-AUC.
+        all_targets.extend(y_truth.cpu().detach().numpy().flatten().tolist())
+        predicted_probabilities.extend(y_pred.cpu().detach().numpy().flatten().tolist())
+
         # Display loss.
         if batch_idx % 10 == 0:
             batch_loss, samples_trained = batch_loss.item(), (batch_idx + 1) * len(X)
             print(f"loss: {batch_loss:>5f}  [{samples_trained:>5d}/{train_set_size:>5d}]")
 
     accuracy = correct_predictions / train_set_size
-    print(f"Train Accuracy: {(100 * accuracy):>0.2f}%")
+    print(f"\nTrain Accuracy: {(100 * accuracy):>0.2f}%")
 
-    return accuracy
+    roc_auc = roc_auc_score(all_targets, predicted_probabilities)
+    print(f"Train ROC-AUC score: {(100 * roc_auc):>0.2f}%")
+
+    return accuracy, roc_auc
 
 
-def _test_epoch(model, dataloader, criterion):
+def _test_epoch(model, dataloader, criterion, device):
     test_set_size = len(dataloader.dataset)
     num_test_batches = len(dataloader)
     test_loss, correct_predictions = 0, 0
 
+    all_targets = list()
+    predicted_probabilities = list()
+
     model.eval()
     with torch.no_grad():
         for X, y_truth in dataloader:
-            y_truth = y_truth.unsqueeze(1)
+            X = X.to(device)
+            y_truth = y_truth.unsqueeze(1).to(device)
             y_pred = model(X)
             test_loss += criterion(y_pred, y_truth)
             correct_predictions += (y_truth == (y_pred >= 0.5)).type(torch.float).sum().item()
 
+            all_targets.extend(y_truth.cpu().detach().numpy().flatten().tolist())
+            predicted_probabilities.extend(y_pred.cpu().detach().numpy().flatten().tolist())
+
     test_loss /= num_test_batches
     accuracy = correct_predictions / test_set_size
-    print(f"Test Accuracy: {(100 * accuracy):>0.2f}%\n"
-          f"Test Loss: {test_loss:>5f}\n")
+    print(f"\nTest Accuracy: {(100 * accuracy):>0.2f}%")
 
-    return accuracy
+    roc_auc = roc_auc_score(all_targets, predicted_probabilities)
+    print(f"Test ROC-AUC score: {(100 * roc_auc):>0.2f}%")
+
+    print(f"Test Loss: {test_loss:>5f}\n")
+
+    return accuracy, roc_auc
 
 
-def _model_dump(model, train_accuracy, test_accuracy, train_config, **kwargs):
+def _model_dump(model, train_accuracy, test_accuracy, train_roc_auc, test_roc_auc, train_config, **kwargs):
     if not os.path.isdir(train_config.save_weights_dir):
         os.mkdir(train_config.save_weights_dir)
 
     torch.save(model.state_dict(), os.path.join(train_config.save_weights_dir, f"{datetime.datetime.now()}--"
-                                                                               f"train-accuracy:{train_accuracy}--"
-                                                                               f"test-accuracy:{test_accuracy}--"
+                                                                               f"train-accuracy:{train_accuracy:.4f}--"
+                                                                               f"test-accuracy:{test_accuracy:.4f}--"
+                                                                               f"train-roc-auc:{train_roc_auc:.4f}--"
+                                                                               f"test-roc-auc:{test_roc_auc:.4f}--"
                                                                                f"{train_config.model_name}--"
                                                                                f"pretrained:{train_config.pretrained}--"
                                                                                f"batch-size:{train_config.batch_size}--"
@@ -105,39 +134,41 @@ def main(train_config):
         shuffle=True
     )
 
-    test_dataset = PlumesDataset(
-        root_dir=train_config.root_dir,
-        is_train=False,
-        augment=False  # We never augment test-data.
-    )
-
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=int(train_config.batch_size)
-    )
+    # test_dataset = PlumesDataset(
+    #     root_dir=train_config.root_dir,
+    #     is_train=False,
+    #     augment=False  # We never augment test-data.
+    # )
+    #
+    # test_loader = DataLoader(
+    #     test_dataset,
+    #     batch_size=int(train_config.batch_size)
+    # )
 
     # Dynamically import model by its name.
     module = __import__("model")
     model_class = getattr(module, train_config.model_name)
-    model = model_class(pretrained=train_config.pretrained).double().to(device)
+    model = model_class(pretrained=train_config.pretrained).double()
+    model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=float(train_config.learning_rate))
     criterion = nn.BCEWithLogitsLoss()
 
     # Launch model training.
-    best_test_accuracy = .0
+    # best_test_accuracy = .0
 
     for epoch in range(int(train_config.epochs)):
         print(f"Epoch {epoch + 1}\n-------------------------------")
-        train_accuracy = _train_epoch(model, train_loader, criterion, optimizer)
-        test_accuracy = _test_epoch(model, test_loader, criterion)
+        train_accuracy, train_roc_auc = _train_epoch(model, train_loader, criterion, optimizer, device)
+        # test_accuracy, test_roc_auc = _test_epoch(model, test_loader, criterion, device)
+        test_accuracy, test_roc_auc = 0, 0
 
-        if test_accuracy >= best_test_accuracy:
-            best_test_accuracy = test_accuracy
-            _model_dump(model, train_accuracy, test_accuracy, train_config, epoch=epoch)
+        # if test_accuracy >= best_test_accuracy:
+        #     best_test_accuracy = test_accuracy
+        #     _model_dump(model, train_accuracy, test_accuracy, train_roc_auc, test_roc_auc, train_config, epoch=epoch)
 
         if epoch % int(train_config.save_weights_freq) == 0:
-            _model_dump(model, train_accuracy, test_accuracy, train_config, epoch=epoch)
+            _model_dump(model, train_accuracy, test_accuracy, train_roc_auc, test_roc_auc, train_config, epoch=epoch)
 
     print("Done!")
 
