a9ebbc1e793608ec7071b9835a7cbb69fa078036
Liza Barysheva <baryshevaes.main@gmail.com>
2023-10-12 15:21:04 +0200

    work on CleanR view

--- app/streamlit-app.py
+++ app/streamlit-app.py
@@ -1,39 +1,22 @@
 import streamlit as st
 from methane_detect import methane_detection
 from map_app import historical_data
+from clean_r import cleanr_display
 
-# Set the page background color
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #FFFAF1;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-# Set the sidebar background color
-st.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        background-color: #B3F502;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
 def main():
     st.sidebar.title("Choose Display")
-    display_option = st.sidebar.radio("Select an option", ("Methane Detection", "Historical Data"))
+    display_option = st.sidebar.radio(
+        "Select an option", ("Methane Detection", "Data Analysis", "CleanR")
+    )
 
-    if display_option == "Historical Data":
+    if display_option == "Data Analysis":
         historical_data()
     elif display_option == "Methane Detection":
         methane_detection()
+    elif display_option == "CleanR":
+        cleanr_display()
+
 
 if __name__ == "__main__":
     main()
