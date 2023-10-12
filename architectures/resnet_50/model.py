982b1fa8e578f749641315a7221fa89f36a77df5
Alekseyev.Mykyta <mykyta.alekseyev@hneu.net>
2023-10-12 16:21:00 +0200

    Extracted ClassificationHead class to the separate module to avoid code-redundancy.

--- architectures/resnet_50/model.py
+++ architectures/resnet_50/model.py
@@ -1,25 +1,8 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 from torchvision.models import resnet50
 
-
-class ClassificationHead(nn.Module):
-    def __init__(self, in_features, n_classes):
-        super().__init__()
-        self.linear_1 = nn.Linear(in_features=in_features, out_features=1024)
-        self.linear_2 = nn.Linear(in_features=1024, out_features=128)
-        self.linear_3 = nn.Linear(in_features=128, out_features=n_classes)
-
-    def forward(self, x):
-        x = self.linear_1(x)
-        x = F.relu(x)
-
-        x = self.linear_2(x)
-        x = F.relu(x)
-
-        logit = self.linear_3(x)
-        return logit
+from ..cls_head import ClassificationHead
 
 
 class ResNet50(nn.Module):
