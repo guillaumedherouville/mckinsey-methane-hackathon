17f0e6b8ea3b066ffef6e92ccf152a7db580f3b8
guillaume <adherouville@outlook.com>
2023-10-12 20:46:58 +0200

    black + other small fix

--- app/methane_detect.py
+++ app/methane_detect.py
@@ -8,6 +8,7 @@ import io
 import rasterio
 import cv2
 
+
 def image_choice():
     """
     Gives option of image source to the user : use a demo file or upload a new one
@@ -16,7 +17,10 @@ def image_choice():
         str: choice of image source
     """
     st.sidebar.markdown("### :arrow_right: Image source")
-    dataSource = st.sidebar.radio("Please select the source of your image :", ["Demo", "Upload"],)
+    dataSource = st.sidebar.radio(
+        "Please select the source of your image :",
+        ["Demo", "Upload"],
+    )
     return dataSource
 
 
@@ -28,16 +32,20 @@ def image_demo():
         str: path to the chosen demo image
     """
     demo_image_examples = {
-    "No plume 1": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_2384.tif",
-    "No plume 2": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_4690.tif",
-    "No plume 3": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_5510.tif",
-
-    "Plume 1": "./data/dataset/train_data/images/plume/20230101_methane_mixing_ratio_id_4928.tif",
-    "Plume 2": "./data/dataset/train_data/images/plume/20230102_methane_mixing_ratio_id_1465.tif",
-    "Plume 3": "./data/dataset/train_data/images/plume/20230102_methane_mixing_ratio_id_4928.tif",}
-    file_path_idx = st.selectbox("Select a demo image from the list", list(demo_image_examples.keys()),)
+        "No plume 1": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_2384.tif",
+        "No plume 2": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_4690.tif",
+        "No plume 3": "./data/dataset/train_data/images/no_plume/20230101_methane_mixing_ratio_id_5510.tif",
+        "Plume 1": "./data/dataset/train_data/images/plume/20230101_methane_mixing_ratio_id_4928.tif",
+        "Plume 2": "./data/dataset/train_data/images/plume/20230102_methane_mixing_ratio_id_1465.tif",
+        "Plume 3": "./data/dataset/train_data/images/plume/20230102_methane_mixing_ratio_id_4928.tif",
+    }
+    file_path_idx = st.selectbox(
+        "Select a demo image from the list",
+        list(demo_image_examples.keys()),
+    )
     file_path = demo_image_examples[file_path_idx]
     return file_path
+
 
 def image_upload(input_type):
     """
@@ -50,7 +58,7 @@ def image_upload(input_type):
     st.title("Methane Detection")
     if input_type == "Upload":
         source_file = st.file_uploader("Upload a TIFF Image", type=["tif", "tiff"])
-    else : 
+    else:
         source_file = image_demo()
 
     if source_file is not None:
@@ -59,11 +67,11 @@ def image_upload(input_type):
         array_plot = np.array(tiff_image)
         fig, ax = plt.subplots(figsize=(10, 10))
         ax.imshow(array_plot, cmap="gray")
-        ax.axis('off')
+        ax.axis("off")
         plt.tight_layout()
         buf = io.BytesIO()
-        fig.savefig(buf, format="png", bbox_inches='tight', pad_inches = 0.05)
-        
+        fig.savefig(buf, format="png", bbox_inches="tight", pad_inches=0.05)
+
         # Normalize the image
         min_val = array_plot.min()
         max_val = array_plot.max()
@@ -71,6 +79,7 @@ def image_upload(input_type):
         return image
     else:
         return None
+
 
 def model_choice():
     """
@@ -80,14 +89,15 @@ def model_choice():
         str: The selected model's filename.
     """
     with st.sidebar:
-        st.text('')
-        
+        st.text("")
+
         # Model choice
         st.markdown("### :arrow_right: Model")
-        model = st.selectbox(
-            label = "Select the model",
-            options = (os.listdir("model/")))
+        models = [model for model in os.listdir("model/")]
+        models.remove('__init__.py')
+        model = st.selectbox(label="Select the model", options=(models))
     return model
+
 
 def prediction(model, image):
     """
@@ -103,17 +113,18 @@ def prediction(model, image):
     # This adds for a status bar
     with st.status("Predicting label", expanded=True) as status:
         st.header(model)
-        
+
         # Load the selected model and predict label
         running_model = keras.models.load_model("model/" + model)
         predicted_label = round((running_model.predict(np.array([image]))[0][0]), 5)
-        label = (1 if predicted_label>=0.5 else 0)
-        
+        label = 1 if predicted_label >= 0.5 else 0
+
         # Display prediction
         st.subheader(f"- Predicted Label: {label} ")
         st.subheader(f"- Predicted Probability: {predicted_label}")
 
         return running_model
+
 
 def heatmap_box(running_model, image):
     """
@@ -124,42 +135,47 @@ def heatmap_box(running_model, image):
         image (numpy.ndarray): The input image as a NumPy array.
     """
     from heatmap import heatmap
+
     # Find the last convolutional layer in the model
-    # get last layer
 
     last_conv_layer = None
     for layer in running_model.layers[::-1]:
         if isinstance(layer, keras.layers.Conv2D):
             last_conv_layer = layer
-            
+
     # Generate the heatmap
     heatmap_generator = heatmap()
-    heatmap_image = heatmap_generator.make_heatmap(np.array([image]), running_model, last_conv_layer.name)
-    heatmap_image = cv2.resize(heatmap_image,(64, 64))
+    heatmap_image = heatmap_generator.make_heatmap(
+        np.array([image]), running_model, last_conv_layer.name
+    )
+    heatmap_image = cv2.resize(heatmap_image, (64, 64))
 
     # Display the original image and heatmap
     fig, ax = plt.subplots(figsize=(10, 10))
-    ax.imshow(image/100, cmap="gray")
-    ax.axis('off')
-    agree = st.checkbox('HeatMap') 
+    ax.imshow(image / 100, cmap="gray")
+    ax.axis("off")
+    agree = st.checkbox("HeatMap")
 
     # Add a checkbox to allow users to toggle the heatmap overlay
     if agree:
         ax.imshow(heatmap_image, alpha=0.4)
         buffed = io.BytesIO()
-        fig.savefig(buffed, format="png", bbox_inches='tight', pad_inches = 0.05)
+        fig.savefig(buffed, format="png", bbox_inches="tight", pad_inches=0.05)
         st.image(buffed)
-        st.markdown("The HeatMap provides insights into which parts of \
+        st.markdown(
+            "The HeatMap provides insights into which parts of \
                 an image were most influential in determining\
-                 the model's prediction")
+                 the model's prediction"
+        )
     else:
         buffed = io.BytesIO()
-        fig.savefig(buffed, format="png", bbox_inches='tight', pad_inches = 0.05)
+        fig.savefig(buffed, format="png", bbox_inches="tight", pad_inches=0.05)
         st.image(buffed)
-    
+
+
 def methane_detection():
     """
-    Perform methane detection by selecting a model, 
+    Perform methane detection by selecting a model,
     choosing an image (demo or upload), and displaying results.
     """
     source_image = image_choice()
--- training_pipelines/train_keras.py
+++ training_pipelines/train_keras.py
@@ -5,36 +5,37 @@ from tensorflow.keras import layers
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-    
+
 def define_model():
     """Define the keras sequential model
 
     Returns:
         tf.keras.model: The model to train
     """
-    model = tf.keras.Sequential([
-        # Convolutional layers
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1)),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(128, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Flatten(),
-        
-        # Fully connected layers
-        layers.Dense(128, activation='relu'),
-        layers.Dropout(0.5),  
-        layers.Dense(64, activation='relu'),
-        layers.Dropout(0.5),
-        layers.Dense(1, activation='sigmoid') 
-    ])
+    model = tf.keras.Sequential(
+        [
+            # Convolutional layers
+            layers.Conv2D(32, (3, 3), activation="relu", input_shape=(64, 64, 1)),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(128, (3, 3), activation="relu"),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+            # Fully connected layers
+            layers.Dense(128, activation="relu"),
+            layers.Dropout(0.5),
+            layers.Dense(64, activation="relu"),
+            layers.Dropout(0.5),
+            layers.Dense(1, activation="sigmoid"),
+        ]
+    )
 
-    model.compile(optimizer='adam',
-                loss='binary_crossentropy',  
-                metrics=[tf.keras.metrics.AUC()])
+    model.compile(
+        optimizer="adam", loss="binary_crossentropy", metrics=[tf.keras.metrics.AUC()]
+    )
     return model
- 
+
 
 def main(train_args):
     """Train the model and save it in models directory
@@ -43,37 +44,61 @@ def main(train_args):
         train_args (argparse.ArgumentParser): Dictionary for terminal arguments
     """
     # Load and preprocess your data
-    data = LoadData(metadata_path=train_args.metadata_path,
-                image_data_path=train_args.image_data_path)
+    data = LoadData(
+        metadata_path=train_args.metadata_path,
+        image_data_path=train_args.image_data_path,
+    )
 
     X, y = data.get_train_data()
     X = data.normalize_data(X)
     # Split your data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=train_args.val_size, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=train_args.val_size, random_state=42
+    )
     if train_args.augment:
-        X_train, y_train = data.augment_data(X_train, y_train, batch_size=train_args.aug_batch)
+        X_train, y_train = data.augment_data(
+            X_train, y_train, batch_size=train_args.aug_batch
+        )
     # Create and compile your Keras model
     model = define_model()
 
     # Train and evaluate the model
-    model.fit(X_train, y_train, epochs=train_args.epochs, batch_size=train_args.batch_size, validation_data=(X_test, y_test))
-    
+    model.fit(
+        X_train,
+        y_train,
+        epochs=train_args.epochs,
+        batch_size=train_args.batch_size,
+        validation_data=(X_test, y_test),
+    )
+
     model.save(f"{train_args.model_path}{train_args.model_name}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train the model from the Terminal")
     parser.add_argument("model_name", help="name the model")
-    parser.add_argument("--metadata_path", default="data/dataset/train_data/metadata.csv")
+    parser.add_argument(
+        "--metadata_path", default="data/dataset/train_data/metadata.csv"
+    )
     parser.add_argument("--image_data_path", default="data/dataset/train_data/")
-    parser.add_argument("--model_path", default="model/", help="directory where models are stored")
+    parser.add_argument(
+        "--model_path", default="model/", help="directory where models are stored"
+    )
     parser.add_argument("--val_size", help="test split ratio ", default=0.2)
     parser.add_argument("--epochs", help="number of epochs in the model", default=10)
-    parser.add_argument("--batch_size", help="batch size used in model training", default=32)
-    parser.add_argument("--group_split", help="Specifying this argument leads to group splitting",
-                        action="store_true")
-    parser.add_argument("--augment", action="store_true", help="Whether to augment data or not")
-    parser.add_argument("--aug_batch", help="batch size of data augmentation", default=32)
+    parser.add_argument(
+        "--batch_size", help="batch size used in model training", default=32
+    )
+    parser.add_argument(
+        "--group_split",
+        help="Specifying this argument leads to group splitting",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--augment", action="store_true", help="Whether to augment data or not"
+    )
+    parser.add_argument(
+        "--aug_batch", help="batch size of data augmentation", default=32
+    )
     args = parser.parse_args()
     main(args)
-    
