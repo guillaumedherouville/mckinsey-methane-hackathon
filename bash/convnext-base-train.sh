1c84455ceb978f5d9c0d47fedd7cb98396c4690a
mykytaalekseiev <alekseyevnf@gmail.com>
2023-10-13 09:46:06 +0200

    Fixing bash scripts

--- bash/convnext-base-train.sh
+++ bash/convnext-base-train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python training_scripts/training_pipeline.py ConvNeXtBase --pretrained --augment --learning-rate 1e-4 --batch-size 64 --epochs 50 --root-dir ./data/dataset_split --save-weights-dir ./weights/convnext-base --save-weights-freq 2
+python training_pipelines/training_pipeline_torch.py ConvNeXtBase --pretrained --augment --learning-rate 1e-4 --batch-size 64 --epochs 50 --root-dir ./data/dataset_split --save-weights-dir ./weights/convnext-base --save-weights-freq 2
--- bash/resnet-50-train.sh
+++ bash/resnet-50-train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python training_scripts/training_pipeline.py ResNet50 --pretrained --augment --learning-rate 1e-4 --batch-size 64 --epochs 50 --root-dir ./data/dataset_split --save-weights-dir ./weights/resnet-50 --save-weights-freq 2
+python training_pipelines/training_pipeline_torch.py ResNet50 --pretrained --augment --learning-rate 1e-4 --batch-size 64 --epochs 50 --root-dir ./data/dataset_split --save-weights-dir ./weights/resnet-50 --save-weights-freq 2
